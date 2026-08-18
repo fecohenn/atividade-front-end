55ace6ae52ba4956ce51b7e396afceb14986aeaf
Luiz Felipe <filiplui435@gmail.com>
2026-08-18 20:01:25 -0300

    Add files via upload

---
+++ arquivo.py
@@ -0,0 +1,26 @@
+altura = float(input("Digite sua altura: "))
+genero = int(input("Digite seu gênero (1 para masculino, 2 para feminino): "))
+altura = [altura]
+genero = [genero]
+masculino = []
+feminino = []
+pessoas_femininos = 0
+media_masculino = 0
+media = 0
+maior = 0
+menor = 0
+for i in range(5):
+    altura.append(float(input("Digite sua altura: ")))
+    genero.append(int(input("Digite seu gênero (1 para masculino, 2 para feminino): ")))
+    if genero[i] == 2:
+        pessoas_femininos += 1
+    if genero[i] == 1:
+        masculino.append(altura[i])
+media = sum(altura) / len(altura)
+maior = max(altura)
+menor = min(altura)
+media_masculino = sum(masculino) / len(masculino)
+print ("A media masculina é: ", media_masculino)
+print("A quantidade de pessoas do gênero feminino é: ", pessoas_femininos)
+print("A maior altura é: ", maior)
+print("A menor altura é: ", menor)
